fa7f246a02348bb29e46eaeb7a2613e28c9ec61c
And11v12 <androidvas228@mail.ru>
2023-11-11 13:45:40 +0300

    g
    g

--- Base.py
+++ Base.py
@@ -1,14 +1,24 @@
 import pygame
 import random
 import time
-
-
+from pygame.locals import (
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
+pygame.mixer.init()
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 700
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+ADD_ENEMY = pygame.USEREVENT + 1
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.image = pygame.image.load('')
+        self.image = pygame.image.load('3.png')
         self.image = pygame.transform.scale(self.image, ())
         self.standard_image = pygame.image.load('').convert()
         self.standard_image = pygame.transform.scale(self.image, (60, 25))
@@ -16,9 +26,27 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
 
-
-class Game(object):
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    def __init__(self):
+class Game():
+    def __init__(self, screen):
         self.speed_game = 1
+        self.screen = screen
+        pygame.display.update()
+    def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+Player()
+Game(screen)
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            run = False
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                run = False
+        #elif event.type == ADD_ENEMY:
+        #   new_enemy = Enemy()
+        #  all_sprites.add(new_enemy)
+        # enemies.add(new_enemy)
