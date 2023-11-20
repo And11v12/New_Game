27016b95c95112c5901f4a35484fa733f8de7362
And11v12 <androidvas228@mail.ru>
2023-11-20 18:00:16 +0300

    6
    6

--- Base.py
+++ Base.py
@@ -18,6 +18,7 @@ FPS = 60
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 ADD_ENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADD_ENEMY, random.randrange(300, 700))
 
 
 class Player(pygame.sprite.Sprite):
@@ -54,20 +55,28 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super(Enemy, self).__init__()
         self.image = pygame.Surface((20, 20))
-        self.col = (10, 10, 10)
+        self.col = (200, 200, 200)
         self.image.fill(self.col)
         self.rect = self.image.get_rect()
         self.rect.x = SCREEN_WIDTH
         self.rect.y = random.randint(0, SCREEN_HEIGHT)
-        self.speed = 7
+        self.speed = 4
 
     def update(self):
         self.rect.x -= self.speed
         if self.rect.left < 0:
             self.kill()
 
-
-
+class Bullet():
+    def __init__(self, posX, posY):
+        self.image = pygame.Surface((10, 10))
+        self.col = (120, 20, 20)
+        self.image.fill(self.col)
+        self.rect = self.image.get_rect()
+    def update(self):
+        self.rect.X += 5
+        if self.rect.right < 0:
+            self.kill()
 
 class Game():
     def __init__(self, screen, ADD_ENEMY):
@@ -76,7 +85,7 @@ class Game():
         self.all_sprites = pygame.sprite.Group()
         self.all_sprites.add(self.player)
         self.ADD_ENEMY = ADD_ENEMY
-        enemies = pygame.sprite.Group()
+        self.enemies = pygame.sprite.Group()
 
 
     def update(self):
@@ -85,15 +94,19 @@ class Game():
                 pygame.quit()
             elif event.type == ADD_ENEMY:
                 enemy = Enemy()
-                all_sprites.add(enemy)
-                enemies.add(enemy)
-                enemy.update()
+                self.all_sprites.add(enemy)
+                self.enemies.add(enemy)
+        self.enemies.update()
         self.all_sprites.update()
         for e in self.all_sprites:
             self.screen.blit(e.image, e.rect)
+        if pygame.sprite.spritecollideany(self.player, self.enemies):
+            self.player.kill()
+            pygame.guit()
+            run = False
 
-game = Game(screen, ADD_ENEMY)
 run = True
+game = Game(screen, ADD_ENEMY)
 while run:
     clock.tick(FPS)
     screen.fill((0, 0, 40))
