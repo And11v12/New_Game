967dcbc6b670284c52e431bf04cabce842ea4d3a
And11v12 <androidvas228@mail.ru>
2023-11-24 17:50:07 +0300

    7
    7(недоделанное задание)

--- Base.py
+++ Base.py
@@ -1,7 +1,11 @@
+import sys
+
 import pygame
 import random
 import time
+import sys
 from pygame.locals import (
+    K_SPACE,
     K_ESCAPE,
     KEYDOWN,
     QUIT,
@@ -22,7 +26,7 @@ pygame.time.set_timer(ADD_ENEMY, random.randrange(300, 700))
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, all_sprites, bullets):
         super(Player, self).__init__()
         self.image = pygame.Surface((20, 20))
         self.col = (100, 100, 100)
@@ -30,6 +34,11 @@ class Player(pygame.sprite.Sprite):
         self.image.fill(self.col)
         self.rect.x = 50
         self.rect.y = 50
+        self.bullets = bullets
+        self.all_sprites = all_sprites
+        self.get_ticks = pygame.time.get_ticks()
+        self.speed = 4
+
 
     def update(self):
         keyUpdate = pygame.key.get_pressed()
@@ -41,6 +50,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.move_ip(-5, 0)
         if keyUpdate[K_RIGHT]:
             self.rect.move_ip(5, 0)
+        if keyUpdate[K_SPACE]:
+            self.shoot()
         if self.rect.right > SCREEN_WIDTH:
             self.rect.right = SCREEN_WIDTH
         if self.rect.left < 0:
@@ -50,6 +61,13 @@ class Player(pygame.sprite.Sprite):
         if self.rect.top < 0:
             self.rect.top = 0
 
+    def shoot(self):
+        self.now = pygame.time.get_ticks()
+        if self.now - self.get_ticks > self.speed:
+            self.get_ticks = self.now
+            bullet = Bullet(self.rect.x, self.rect.y)
+            self.all_sprites.add(bullet)
+            self.bullets.add(bullet)
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
@@ -67,12 +85,19 @@ class Enemy(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.kill()
 
-class Bullet():
-    def __init__(self, posX, posY):
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, posX, posY, *groups: _Group):
+        super().__init__(*groups)
         self.image = pygame.Surface((10, 10))
         self.col = (120, 20, 20)
         self.image.fill(self.col)
         self.rect = self.image.get_rect()
+        self.speed = 4
+        self.rect.x = posX
+        self.rect.y = posY
+
+
+
     def update(self):
         self.rect.X += 5
         if self.rect.right < 0:
@@ -81,29 +106,33 @@ class Bullet():
 class Game():
     def __init__(self, screen, ADD_ENEMY):
         self.screen = screen
-        self.player = Player()
         self.all_sprites = pygame.sprite.Group()
-        self.all_sprites.add(self.player)
         self.ADD_ENEMY = ADD_ENEMY
         self.enemies = pygame.sprite.Group()
+        self.bullets = pygame.sprite.Group()
+        self.player = Player(self.all_sprites, self.bullets)
+        self.all_sprites.add(self.player)
 
 
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                sys.exit()
             elif event.type == ADD_ENEMY:
                 enemy = Enemy()
                 self.all_sprites.add(enemy)
                 self.enemies.add(enemy)
         self.enemies.update()
         self.all_sprites.update()
+        self.bullets.update()
         for e in self.all_sprites:
             self.screen.blit(e.image, e.rect)
         if pygame.sprite.spritecollideany(self.player, self.enemies):
             self.player.kill()
-            pygame.guit()
-            run = False
+            pygame.quit()
+            sys.exit()
+
 
 run = True
 game = Game(screen, ADD_ENEMY)
